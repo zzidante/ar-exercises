51dcb741e8f5d3fb37b4a379f8bef8fc594f6b4b
Mel <zzidante@gmail.com>
2017-11-07 22:17:56 +0000

    loop through stores based on what apparel they carry and/or annual revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,17 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surr_store= Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+whis_store= Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+yale_store= Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+
+# ("orders_count = '2'")
+
+puts "Men's Stores"
+@mens_stores.each {|store| puts store.name, store.annual_revenue}
+puts "Women's Stores"
+@womens_stores.each {|store| puts store.name, store.annual_revenue}
