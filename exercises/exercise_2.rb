591058748bc3f6c0d21c5589879c825f4d0688d1
Mel <zzidante@gmail.com>
2017-11-07 21:38:09 +0000

    changed Store 1 name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,15 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store_1 = Store.find_by(id: 1)
+
+puts @store_1.name
+
+@store_2 = Store.find_by(id: 2)
+
+puts @store_2.name
+
+@store_1 = Store.find_by(id: 1)
+
+puts @store_1.update(name: "Port Moody")
